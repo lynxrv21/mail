8ef89ae5c7826662f5cbc206b6fdc80a5ed3e12f
lynxrv21 <dba5bbef@opayq.com>
2015-05-26 00:32:10 +0300

    one more step - re

--- mail.py
+++ mail.py
@@ -8,38 +8,47 @@ from email.utils import parseaddr
 from email import message_from_file
 
 if __name__ == '__main__':
-
-    # print sys.argv[1:]
     try:
         path = sys.argv[1]
-        # print path
         try:
             msg = message_from_file(open(path, "r"))
             # print msg
             if msg.is_multipart():
-                for payload in msg.get_payload():
-                    print payload.get_payload()
+                print "Your file seems to be multipart message... I'd better skip it"
             else:
-                print msg.get_payload()
+                Body = msg.get_payload()
+                headers = Parser().parse(open(path, "r"))
 
-            headers = Parser().parse(open(path, "r"))
+                if not headers["Subject"] and headers["From"] and headers["To"] and Body:
+                    print "! Error: check your file, noting to do here"
+                else:
+                    if not Body:
+                        print "! Warning: no 'Body' field in your file"
+                    elif not headers["From"]:
+                        print "! Warning: no 'From' field in your file"
+                    elif not headers["To"]:
+                        print "! Warning: no 'To' field in your file"
 
-            if not headers["Subject"] & headers["From"] & headers["To"] :
-                print "not"
-            else:
-                print "is"
+                    raw_from = parseaddr(headers["From"])
+                    raw_to = parseaddr(headers["To"])
+                    raw_subject = parseaddr(headers["Subject"])
+                    raw_cc = parseaddr(headers["Cc"])
+                    raw_bcc = parseaddr(headers["Bcc"])
+                    print raw_from
+                    print raw_to
+                    print raw_subject
 
-            From = parseaddr(headers["From"])
-            print "From: %s" % From[1]
-            print "To: %s" % headers["To"]
-            print "Subject: %s" % headers["Subject"]
+                    mail_pattern = re.compile(r"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$")
+                    url_pattern = re.compile(r"")
 
-
+                    for item in raw_from:
+                        if re.match(mail_pattern, item):
+                            print item
 
         except IOError:
-            print "Error: file or path not found"
+            print "! Error: file or path not found"
     except IndexError:
-        print "Error: please, enter message file path"
+        print "! Error: please, enter message file path"
 
         # r'^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$'
         # \s?[A-Z0-9_-+%]+@[A-Z0-9_-+%]+\.[A-Z0-9]\s?
